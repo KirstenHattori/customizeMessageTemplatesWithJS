69b7559264aa3bf8a03f2e5de6dafafe954d2afe
Kirsten <kirstenmhattori@gmail.com>
2023-05-04 18:06:05 -0600

    wrote pseudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+// STEP 1: create the data structure for the people and panel/workshop details
+  //person: name, \*role (panellist, moderator, host)*\
+  //event: title, type (panel, workshop)
+
+
+// STEP 2: create a function with parameters for outputting a customized message
+  //uses template literals probably
+  //Hi ${name},
+// I'm Kirsten, a front end dev with UX and project mgt experience. Saw that you'll be at DiscoverTechYYC, looking forward to your ${event.title} ${event.type}panel, and hope to connect!
+
+// STEP 3: make a loop to output the message for all 11 people
+
